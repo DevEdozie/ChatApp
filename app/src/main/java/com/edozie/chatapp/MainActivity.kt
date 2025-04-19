f8e42872ea1c013bc1044a8cc762d4812af0b211
Ojiakor Michael <nicholausmichael51@gmail.com>
2025-04-18 20:28:13 -0930

    (bugfix): Auth logic bugs fixed. App now checks for internet connection and empty fields before calling firebase

--- app/src/main/java/com/edozie/chatapp/MainActivity.kt
+++ app/src/main/java/com/edozie/chatapp/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.edozie.chatapp.nav.AppNavigation
+import com.edozie.chatapp.util.NetworkObserver
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var networkObserver: NetworkObserver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppNavigation()
+            AppNavigation(networkObserver)
         }
     }
 }
--- app/src/main/java/com/edozie/chatapp/di/AppModule.kt
+++ app/src/main/java/com/edozie/chatapp/di/AppModule.kt
@@ -1,10 +1,13 @@
 package com.edozie.chatapp.di
 
+import android.content.Context
+import com.edozie.chatapp.util.NetworkObserver
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -16,9 +19,14 @@ object AppModule {
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
 
-
     @Provides
     @Singleton
     fun provideFirebaseFirestore(): FirebaseFirestore =
         FirebaseFirestore.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideNetworkObserver(
+        @ApplicationContext context: Context
+    ): NetworkObserver = NetworkObserver(context)
 }
